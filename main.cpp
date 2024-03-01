3046248b1f1cc427f15d0301c1bbe95eff99961c
Jungsoo Nam <namjungsoo@gmail.com>
2024-03-01 09:06:03 +0900

    Use VK_USE_PLATFORM_WIN32_KHR

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+// before vulkan.h
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int WinMain() {
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
     VkInstanceCreateInfo info = {};
     info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 
